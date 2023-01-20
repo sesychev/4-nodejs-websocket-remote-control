ba6273577a3693cbb19771b6ffd84d625a745921
sesychev <sergeysychou@gmail.com>
2023-01-20 12:15:15 +0300

    feat:  workable websocket server is Implemented

--- remote-control/index.ts
+++ remote-control/index.ts
@@ -1,10 +1,9 @@
 import { wss as websocketServer } from "./src/websocket/websocket";
 import { httpServer } from "./src/http_server/index";
-import { mouse } from "@nut-tree/nut-js";
 
 const HTTP_PORT = 8181;
 
 httpServer.listen(HTTP_PORT);
-console.log(`Start static http server http://localhost:${HTTP_PORT} on the port ${HTTP_PORT}!`);
+console.log(`Start Static server http://localhost:${HTTP_PORT} on the port ${HTTP_PORT}!`);
 
 websocketServer;
