e7378d1c19129f15fa57b12d5e76321375f5508a
sesychev <sergeysychou@gmail.com>
2023-01-20 14:19:07 +0300

    feat:  npm doesn't work

--- remote-control/src/websocket/websocket.ts
+++ remote-control/src/websocket/websocket.ts
@@ -10,13 +10,17 @@ wss.on('connection', (ws) => {
   const duplex = createWebSocketStream(ws, { encoding: 'utf8', decodeStrings: false });
 
   duplex.on('data', async (chunk: object) => {
-    console.log('received: %s', chunk.toString().split(' '));
+    console.log('received: %s', chunk);
+    //
+
   });
+
+  duplex.write('something');
 
   duplex.on('open', () =>{
     console.log('connected');
   });
-  
+
   duplex.on('close', () => {
     console.log('disconnected');
   });
@@ -25,3 +29,9 @@ wss.on('connection', (ws) => {
     console.log(err);
   })
 });
+
+/*
++6 Implemented workable websocket server
++10 Websocket server message handler implemented properly
++10 Websocket server message sender implemented properly
+*/
