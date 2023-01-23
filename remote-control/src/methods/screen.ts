0668c39e1cbf110b9213afaedc22f1b085bf5851
sesychev <sergeysychou@gmail.com>
2023-01-23 17:17:35 +0300

    feat: edit screen method

--- remote-control/src/methods/screen.ts
+++ remote-control/src/methods/screen.ts
@@ -1,18 +1,16 @@
 import { Region, mouse, centerOf, straightTo, screen, imageResource } from '@nut-tree/nut-js';
 import Jimp from 'jimp';
-
+//https://stackoverflow.com/questions/41941151/capture-and-save-image-with-robotjs
 export async function printing() {
-const left = (await mouse.getPosition()).x;
-const top = (await mouse.getPosition()).y;
-const region = new Region(left-100, top+100, 200, 200);
-const image = new Jimp(region.width, region.height);
-console.log(image.bitmap.data)
+  const left = (await mouse.getPosition()).x;
+  const top = (await mouse.getPosition()).y;
+  const region = new Region(left - 100, top + 100, 200, 200);
+  const img = (await screen.grabRegion(region));
+  const image = new Jimp(img.width, img.height)
+  //const image = new Jimp(region.width, region.height); => //console.log(image.bitmap.data.buffer)
+  //image.bitmap.data = img.data;
   const base64 = await image.getBase64Async(Jimp.MIME_PNG);
+  console.log(base64)
+
   return base64;
 }
-/*
-Print screen
-Make print screen command and send image (a base64 buffer of the 200 px square around the mouse position):
-<- prnt_scrn
--> prnt_scrn {base64 string (png buf)}
-*/
