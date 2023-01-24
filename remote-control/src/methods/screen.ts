d38f7db5f0c67c84fc809a988f8e0e1b2e6bfb89
sesychev <sergeysychou@gmail.com>
2023-01-25 00:01:18 +0300

    feat: screen done completely

--- remote-control/src/methods/screen.ts
+++ remote-control/src/methods/screen.ts
@@ -1,21 +1,13 @@
 import { Region, mouse, screen } from '@nut-tree/nut-js';
-import robot from 'robotjs';
 import Jimp from 'jimp';
 //https://stackoverflow.com/questions/41941151/capture-and-save-image-with-robotjs
 export async function printing() {
   const left = (await mouse.getPosition()).x;
   const top = (await mouse.getPosition()).y;
   const region = new Region(left - 100, top + 100, 200, 200);
-  const img = (await screen.grabRegion(region));
-  const context = robot.screen.capture(left - 100, top - 100, 200, 200);
-  const image = new Jimp(200, 200);
-  image.bitmap.data = context.image;
-    image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
-    const red = image.bitmap.data[ idx + 0 ];
-    const blue = image.bitmap.data[ idx + 2 ];
-    image.bitmap.data[ idx + 0 ] = blue;
-    image.bitmap.data[ idx + 2 ] = red;
-  });
+  const grab = (await screen.grabRegion(region));
+  const img = await grab.toRGB();
+  const image = new Jimp({ data: img.data, width: 200, height: 200 });
   const base64 = await image.getBase64Async(Jimp.MIME_PNG);
   console.log(base64)
   return base64;
